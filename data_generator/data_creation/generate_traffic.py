8cb83c302281c786e3260ae5109b1b44e5971254
Noce99 <enrico.mannocci99@gmail.com>
2025-04-30 12:12:22 +0200

    Restored color and added debug!

--- data_generator/data_creation/generate_traffic.py
+++ data_generator/data_creation/generate_traffic.py
@@ -115,8 +115,9 @@ def generate_traffic(carla_ip, rpc_port, tm_port, number_of_vehicles, number_of_
         batch = []
         if hero:
             blueprint = world.get_blueprint_library().find('vehicle.mini.cooper')
-            # blueprint.set_attribute('color', blueprint.get_attribute('color').recommended_values[0])
+            blueprint.set_attribute('color', blueprint.get_attribute('color').recommended_values[0])
             blueprint.set_attribute('role_name', 'hero')
+            print(f"spawn_points = {spawn_points}")
             batch.append(SpawnActor(blueprint, spawn_points[random.randint(0, len(spawn_points) - 1)])
                          .then(SetAutopilot(FutureActor, True, traffic_manager.get_port())))
         for n, transform in enumerate(spawn_points[:]):
