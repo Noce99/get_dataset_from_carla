30513686c244bde96c22ba4d4f6868c2574620e1
Noce99 <enrico.mannocci99@gmail.com>
2025-04-30 11:57:24 +0200

    Updated the get_data.bash script!

--- get_data.bash
+++ get_data.bash
@@ -3,11 +3,11 @@ for i in {1..300}
 do
   echo "Sequence: $i [$current_data]"
   python generate_data.py \
-  --carla_path /home/enrico/Projects/Carla/CARLA_0.9.15/ \
+  --carla_path /home/carla/CarlaUE5/ \
   --town 10 \
   --num_of_vehicle 0 \
   --num_of_walkers 0 \
-  --dataset_path /media/enrico/Enrico_Datasets/carla_events/"$current_data" \
+  --dataset_path /home/carla/"$current_data" \
   --sequence_id "$i"
   exit_code=$?
   if [ $exit_code -eq 99 ]; then
